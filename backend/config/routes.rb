c5c8196c784dc989ddacf3363209ac845daae4e5
Sky Blue <dawadiaakash@gmail.com>
2025-07-31 15:51:35 +0545

    feat(backend): update api routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
-namespace :api do
-  resources :blogs do
-    member do
-      patch 'update_completed'
+  namespace :api do
+    resources :blogs do
+      member do
+        patch 'update_completed'
+      end
+    end
+
+    devise_for :users, skip: [:sessions], controllers: {
+      sessions: 'api/sessions'
+    }
+
+    as :user do
+      post 'login', to: 'api/sessions#create'
+      delete 'logout', to: 'api/sessions#destroy'
     end
   end
-end
 end
