ce7dedce3a547237807ac0462fb56a93ad8be804
Sky Blue <dawadiaakash@gmail.com>
2025-08-02 00:24:58 +0545

    fix(backend): signup via postman

--- backend/app/controllers/users/sessions_controller.rb
+++ backend/app/controllers/users/sessions_controller.rb
@@ -1,3 +1,25 @@
 class Users::SessionsController < Devise::SessionsController
   respond_to :json
+  include RackSessionFix
+
+   private
+
+  def respond_with(resource, _opts = {})
+    render json: {
+       message: 'Logged in sucessfully.',
+      data: resource
+    }, status: :ok
+  end
+
+  def respond_to_on_destroy
+    if current_user
+      render json: {
+        message: "logged out successfully"
+      }, status: :ok
+    else
+      render json: {
+        message: "Couldn't find an active session."
+      }, status: :unauthorized
+    end
+  end
 end
