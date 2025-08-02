adf16aaf008099976c0f9a16e8bce5fb55fd3bad
Sky Blue <dawadiaakash@gmail.com>
2025-08-02 13:08:36 +0545

    feat(backend): add migrations to AddUserToBlogs

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_01_092807) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_02_071840) do
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,4 +33,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_01_092807) do
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "blogs", "users"
 end
