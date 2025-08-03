5ec971d79b102f291ad29f25e3dbf10a3040b62e
Sky Blue <dawadiaakash@gmail.com>
2025-08-03 14:58:13 +0545

    feat(frontend): add PropTypes validation for pageType in Authentication component

--- frontend/src/pages/Authentication.js
+++ frontend/src/pages/Authentication.js
@@ -1,20 +1,27 @@
 import React from "react";
+import PropTypes from 'prop-types'
 
 export const PageType = Object.freeze({
-  LOGIN: 'LOGIN',
-  REGISTER: 'REGISTER'
+  LOGIN: 0,
+  REGISTER: 1,
 });
 
-const Authentication = ({ pageType = PageType.LOGIN }) => {
+
+
+const Authentication = ({ pageType}) => {
   return (
-    <div>
-      {pageType === PageType.LOGIN ? (
-        <h1>Login</h1>
-      ) : (
-        <h1>Register</h1>
-      )}
+    <div className="bg-white">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 py-12">
+            <h3 className="text-2xl font-bold">
+                {(pageType===PageType.LOGIN)? 'Login': 'Register'}
+                 </h3>
+        </div>
     </div>
   );
+};
+
+Authentication.propTypes = {
+    pageType: PropTypes.number.isRequired,
 };
 
 export default Authentication;
